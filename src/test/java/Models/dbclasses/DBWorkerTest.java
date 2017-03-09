dbf4d12afe360b225d974d90367a1f87a917f211
Ramiz A <ramiz.demiurge@gmail.com>
2017-03-09 19:12:06 +0400

    Testing DBWorker class and InitUser method

---
+++ src/test/java/Models/dbclasses/DBWorkerTest.java
@@ -0,0 +1,23 @@
+package Models.dbclasses;
+
+import Models.UserModel.User;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DBWorkerTest
+{
+    @Test
+    public void getConnection() throws Exception
+    {
+        DBWorker dbWorker = new DBWorker();
+        Assert.assertNotEquals(null,dbWorker.getConnection());
+    }
+
+    @Test
+    public void initUser() throws Exception
+    {
+        User user = DBWorker.InitUser(new User("test","test"));
+        Assert.assertTrue(user.getCorrectness());
+    }
+
+}
