99e075a364526a386f007d2fbd01bdd7a472f233
Ramiz A <ramiz.demiurge@gmail.com>
2017-03-09 19:01:36 +0400

    Deleting comments

--- src/main/java/Controllers/MainServlet.java
+++ src/main/java/Controllers/MainServlet.java
@@ -11,9 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-/**
- * Created by adsf on 10.01.2017.
- */
+
 public class MainServlet extends HttpServlet
 {
     @Override
