452ab800bb24a9fcff750118239d6e85079e7b93
Ramiz A <ramiz.demiurge@gmail.com>
2017-03-10 21:32:06 +0400

    Upgrading ArticlesServlet controller

--- src/main/java/Controllers/ArticleServlet.java
+++ src/main/java/Controllers/ArticleServlet.java
@@ -1,21 +1,33 @@
 package Controllers;
 
+import Models.dbclasses.Article;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/**
- * Created by allard on 3/10/17.
- */
 public class ArticleServlet extends HttpServlet
 {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException
     {
-        getServletContext().getRequestDispatcher("/View/article_get.jsp").forward(req, resp);
-        req.getSession().removeAttribute("messages");
+
+        String string_id = req.getParameter("id");
+        if (string_id != null)
+        {
+            int id = Integer.parseInt(string_id.trim());
+            Article article;
+            article = Article.InitArticle(new Article(id));
+            req.getSession().setAttribute("article", article);
+            getServletContext().getRequestDispatcher("/View/article_get.jsp").forward(req, resp);
+
+        } else
+        {
+            req.getSession().setAttribute("messages", "There is no such article");
+            resp.sendRedirect("/index.html");
+        }
     }
 }
