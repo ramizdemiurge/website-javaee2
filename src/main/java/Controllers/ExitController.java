e1f4bd15c5ec0c25e81874a0e7203dd81bfd5d39
Ramiz A <ramiz.demiurge@gmail.com>
2017-03-15 02:41:12 +0400

    Casting ExitServlet to spring-mvc scheme

---
+++ src/main/java/Controllers/ExitController.java
@@ -0,0 +1,25 @@
+package Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@Controller
+@RequestMapping("/exit")
+public class ExitController
+{
+    @RequestMapping(method = RequestMethod.GET)
+    public void RemoveSession(HttpServletRequest req, HttpServletResponse resp) throws IOException
+    {
+        HttpSession mySession = req.getSession();
+        mySession.removeAttribute("username");
+        mySession.removeAttribute("password");
+        resp.sendRedirect("/");
+        //return null;
+    }
+}
