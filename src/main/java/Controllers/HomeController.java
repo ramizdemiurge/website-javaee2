4d1091095728144a00373d8df5a9b9e6f1f388db
Ramiz A <ramiz.demiurge@gmail.com>
2017-03-15 02:06:50 +0400

    spring-mvc, configuring home

--- src/main/java/Controllers/HomeController.java
+++ src/main/java/Controllers/HomeController.java
@@ -1,8 +1,63 @@
 package Controllers;
 
-/**
- * Created by allard on 3/15/17.
- */
+import Models.dbclasses.DBWorker;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+@Controller
+@RequestMapping("/home")
 public class HomeController
 {
+    @RequestMapping(method = RequestMethod.GET)
+    public String printWelcome(HttpServletRequest req, HttpServletResponse resp) throws IOException
+    {
+        HttpSession mySession = req.getSession();
+        String username = (String) mySession.getAttribute("username");
+        String passwd = (String) mySession.getAttribute("passwd");
+        if (username != null && passwd != null)
+        {
+
+            DBWorker worker_ = new DBWorker();
+            boolean status = false;
+            // String query = "select * from users WHERE username=" + login;
+            String query = "select * from users WHERE username=\"" + username + "\"";
+            String[] datas = new String[3];
+            try
+            {
+                Statement statements = worker_.getConnection().createStatement();
+                ResultSet resultsSet = statements.executeQuery(query);
+                while (resultsSet.next())
+                {
+                    String password = resultsSet.getString("password");
+                    datas[0] = resultsSet.getString("reg_date");
+                    datas[1] = resultsSet.getString("name");
+                    if (passwd.equals(password)) status = true;
+                }
+            } catch (SQLException ed)
+            {
+                ed.printStackTrace();
+            }
+            if (status)
+            {
+                mySession.setAttribute("Datas", datas);
+                mySession.setAttribute("username", username);
+                return "home_get";
+            } else
+            {
+                req.getSession().setAttribute("messages","Correct your login/password.");
+                resp.sendRedirect("/index.html");
+            }
+        }
+        resp.sendRedirect("/index.html");
+        return null;
+    }
 }
--- src/main/java/Controllers/HomeServlet.java
+++ src/main/java/Controllers/HomeServlet.java
@@ -1,16 +1,10 @@
 package Controllers;
 
-import Models.dbclasses.DBWorker;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 /**
  * Created by adsf on 14.01.2017.
@@ -20,42 +14,42 @@ public class HomeServlet extends HttpServlet
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
-        HttpSession mySession = req.getSession();
-        String username = (String) mySession.getAttribute("username");
-        String passwd = (String) mySession.getAttribute("passwd");
-        if (!(username.equals(null) || username.equals("") || passwd.equals(null) || passwd.equals("")))
-        {
-
-            DBWorker worker_ = new DBWorker();
-            boolean status = false;
-            // String query = "select * from users WHERE username=" + login;
-            String query = "select * from users WHERE username=\"" + username + "\"";
-            String[] datas = new String[3];
-            try
-            {
-                Statement statements = worker_.getConnection().createStatement();
-                ResultSet resultsSet = statements.executeQuery(query);
-                while (resultsSet.next())
-                {
-                    String password = resultsSet.getString("password");
-                    datas[0] = resultsSet.getString("reg_date");
-                    datas[1] = resultsSet.getString("name");
-                    if (passwd.equals(password)) status = true;
-                }
-            } catch (SQLException ed)
-            {
-                ed.printStackTrace();
-            }
-            if (status)
-            {
-                mySession.setAttribute("Datas", datas);
-                mySession.setAttribute("username", username);
-                getServletContext().getRequestDispatcher("/View/home_get.jsp").forward(req, resp);
-            } else
-            {
-                req.getSession().setAttribute("messages","Correct your login/password.");
-                resp.sendRedirect("/index.html");
-            }
-        }
+//        HttpSession mySession = req.getSession();
+//        String username = (String) mySession.getAttribute("username");
+//        String passwd = (String) mySession.getAttribute("passwd");
+//        if (!(username.equals(null) || username.equals("") || passwd.equals(null) || passwd.equals("")))
+//        {
+//
+//            DBWorker worker_ = new DBWorker();
+//            boolean status = false;
+//            // String query = "select * from users WHERE username=" + login;
+//            String query = "select * from users WHERE username=\"" + username + "\"";
+//            String[] datas = new String[3];
+//            try
+//            {
+//                Statement statements = worker_.getConnection().createStatement();
+//                ResultSet resultsSet = statements.executeQuery(query);
+//                while (resultsSet.next())
+//                {
+//                    String password = resultsSet.getString("password");
+//                    datas[0] = resultsSet.getString("reg_date");
+//                    datas[1] = resultsSet.getString("name");
+//                    if (passwd.equals(password)) status = true;
+//                }
+//            } catch (SQLException ed)
+//            {
+//                ed.printStackTrace();
+//            }
+//            if (status)
+//            {
+//                mySession.setAttribute("Datas", datas);
+//                mySession.setAttribute("username", username);
+//                getServletContext().getRequestDispatcher("/View/home_get.jsp").forward(req, resp);
+//            } else
+//            {
+//                req.getSession().setAttribute("messages","Correct your login/password.");
+//                resp.sendRedirect("/index.html");
+//            }
+//        }
     }
 }
